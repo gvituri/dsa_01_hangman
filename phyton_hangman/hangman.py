1d9fcec94ec987982fc388abf729921523b47703
gvituri <gustavovituri@gmail.com>
2021-09-03 08:29:05 -0300

    Add match class and board

--- phyton_hangman/hangman.py
+++ phyton_hangman/hangman.py
@@ -1,7 +1,76 @@
 import random
 
+class Match():
+
+    match_number = 0
+    board_states = {
+        0: [" ╒══╗ ",
+            "    ║ ",
+            "    ║ ",
+            "    ║ ",
+            "    ║ ",
+            "════╩═"],
+        1: [" ╒══╗ ",
+            " O  ║ ",
+            "    ║ ",
+            "    ║ ",
+            "    ║ ",
+            "════╩═"],
+        2: [" ╒══╗ ",
+            " O  ║ ",
+            " |  ║ ",
+            "    ║ ",
+            "    ║ ",
+            "════╩═"],
+        3: [" ╒══╗ ",
+            " O  ║ ",
+            "/|  ║ ",
+            "    ║ ",
+            "    ║ ",
+            "════╩═"],
+        4: [" ╒══╗ ",
+            " O  ║ ",
+            "/|\ ║ ",
+            "    ║ ",
+            "    ║ ",
+            "════╩═"],
+        5: [" ╒══╗ ",
+            " O  ║ ",
+            "/|\ ║ ",
+            "/   ║ ",
+            "    ║ ",
+            "════╩═"],
+        6: [" ╒══╗ ",
+            " O  ║ ",
+            "/|\ ║ ",
+            "/ \ ║ ",
+            "    ║ ",
+            "════╩═"],
+    }
+
+    def __init__(self, word):
+        self.word = word
+        self.board = self.reset_board()
+
+    def reset_board(self):
+        return Match.board_states[0]
+
+def import_dictionary():
+    with open("dictionary.txt", "rt") as file:
+        dictionary = file.readlines()
+        return dictionary
+
+def pick_random_word(dictionary):
+    return dictionary[random.randint(0, len(dictionary))].strip()
+
 def main():
-    print("start program")
+    dictionary = import_dictionary()
+    print(dictionary)
+    word = pick_random_word(dictionary)
+    print(word)
+    match = Match(word)
+    for line in match.board:
+        print(line)
 
 if __name__ == "__main__":
     main()
